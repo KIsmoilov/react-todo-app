a47e6d88085fa3a7df7c0cdfc01f589f24cabfe3
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-12-07 12:54:28 +0500

    Change the Hashrouter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './functionBased/pages/Home';
 import About from './functionBased/pages/About';
 import Navbar from './functionBased/components/Navbar';
@@ -9,12 +9,12 @@ import './functionBased/App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
-      <Routes>
+      <Router>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-      </Routes>
-    </HashRouter>
+      </Router>
+    </Router>
   </React.StrictMode>,
 );
